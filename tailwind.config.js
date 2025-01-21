0e1a35405d364bb8f59e2788b98d328dee00be74
rofenac <118231149+rofenac@users.noreply.github.com>
2025-01-21 14:00:39 -0800

    Cleaned out Boilerplate Code

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-],
+  ],
   theme: {
     extend: {},
   },
@@ -12,4 +12,3 @@ export default {
     daisyui,
   ],
 }
-
