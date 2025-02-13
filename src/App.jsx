f6a94f4a2248007b75fac2ddddc72a817317ab2c
rofenac <118231149+rofenac@users.noreply.github.com>
2025-02-13 12:56:56 -0800

    Added JSON Reformatter

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { gsap } from "gsap"
 import { useGSAP } from "@gsap/react"
 import HomePage from './pages/homepage'
+import JsonReformatter from './pages/JsonReformatter'
 
 gsap.registerPlugin(useGSAP);
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/jsonreformatter" element={<JsonReformatter />} />
     </Routes>
   )
 }
